41b662820907bc813fc4fdf206ef9d4401348c88
victkch <kochergavictoria@gmail.com>
2022-12-24 13:46:49 +0200

    servers updates - lecture-15

--- servers/WebSocket-server.ts
+++ servers/WebSocket-server.ts
@@ -4,11 +4,29 @@ const webSocket = require("ws");
 const webSocketServer = new webSocket.Server({ server: httpServer });
 
 webSocketServer.on("connection", (ws: typeof webSocket) => {
-  console.log("The WebSocket-server started.");
   ws.send("Hi. This is WebSocket-server!");
-  ws.on("message", (sentMessage: string) => {
-    console.log(`User sent this message - ${sentMessage}`);
-    ws.send(`Hi, there! The message you sent - ${sentMessage}`);
+  if(){
+    //token validation
+    console.log("Connected."); 
+  }
+  else ws.close();
+  ws.on("close", () => console.log("Disonnected."));
+  ws.on("message", (sentJSON: JSON) => {
+    if(){
+      //attack
+      console.log("User attacks another user.");
+    }
+    else if(){
+      //ability
+      console.log("User's ability.");
+    }
+    else if(){
+      //mesage
+      console.log("User sent a message.");
+    }
+    else if(){
+      //restore
+      console.log("Restore user.");
+    }  
   });
-  ws.on("close", () => console.log("The WebSocket-server stopped."));
 });
